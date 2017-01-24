13fd052efaa291266cd7610c228cb24deaf6da69
Luke San Antonio Bialecki <lukesanantonio@gmail.com>
2017-01-24 14:14:27 -0500

    Add Org model and deserializer to Model/ subtree

---
+++ InPassing/InPassing/Model/Org.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InPassing.Model
+{
+  public class Org
+  {
+    public int Id { get; private set; }
+    public string Name { get; private set; }
+    public Org(int id, string name)
+    {
+      Id = id;
+      Name = name;
+    }
+    public static Org FromDynamic(dynamic obj)
+    {
+      return new Org(obj.id, obj.name);
+    }
+  }
+}
