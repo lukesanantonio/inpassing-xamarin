de5baec4bddd5088c5cef917212eaacadda4b813
Luke San Antonio Bialecki <lukesanantonio@gmail.com>
2017-02-15 23:54:37 -0500

    Update OrgHome to support org name, greeting and passes
    It's pretty sweet, but doesn't have much functionality. It also shows
    all passes owned by the user, which could be really confusing with
    enough passes, etc.

--- InPassing/InPassing/OrgHome.xaml.cs
+++ InPassing/InPassing/OrgHome.xaml.cs
@@ -8,18 +8,53 @@ using Xamarin.Forms;
 
 namespace InPassing
 {
+    class PassView
+    {
+        public string Day { get; set; }
+        public int Spot { get; set; }
+
+        public PassView(String day, int spot)
+        {
+            Day = day;
+            Spot = spot;
+        }
+
+    }
     public partial class OrgHome : ContentPage
     {
         public Model.Org Org { get; set; }
+        public List<Model.PassShallow> Passes { get; set; }
+
+        public Backend Backend;
         public OrgHome(Model.Org org)
         {
             InitializeComponent();
             Org = org;
+        }
 
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            Backend = ((App)Application.Current).AppBackend;
             this.BindingContext = new
             {
-                Name = Org.Name
+                OrgName = Org.Name,
+                OrgGreeting = Backend.GetCurrentDaystate(Org.Id).Greeting
             };
+
+            Passes = Backend.GetPasses();
+
+            // TODO: Filter out passes not from this org
+            var views = Passes.Select(p =>
+            {
+                return new PassView(
+                    Backend.GetDaystate(p.OrgId, p.StateId).Identifier,
+                    p.SpotNum
+                );
+            }).ToList<PassView>();
+
+            passesView.ItemsSource = views;
         }
     }
 }
